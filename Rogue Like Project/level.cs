691070f086bb8f460fcdce8f3e1ef7f64cc176b9
Draken93 <dana.dohmen@gmx.de>
2013-08-08 12:36:47 +0200

    Level Konny

---
+++ Rogue Like Project/level.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rogue_Like_Project
+{
+    public static class level
+    {
+        static void LvL()
+        {
+            int counter = 0;
+            string line;
+
+            
+            System.IO.StreamReader file = new System.IO.StreamReader("D:\\lvl1.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
